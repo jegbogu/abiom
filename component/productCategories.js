7b04f83796771fea271304b567b1faea7ec4cc52
Chinyere <chinyereunamba17@gmail.com>
2025-06-09 17:45:55 +0100

    some changes to home page

--- component/productCategories.js
+++ component/productCategories.js
@@ -1,37 +1,74 @@
-
-import { useRef } from 'react';
-import classes from './productCategories.module.css';
-import { useRouter } from 'next/router';
+import { useRef } from "react";
+import classes from "./productCategories.module.css";
+import { useRouter } from "next/router";
 
 const products = [
-  { id: 1, name: 'Home Appliances', image: '/blender.avif',link:`/product_categories/home appliences` },
-  { id: 2, name: 'Food and Soup', image: '/Whole Ogbonor seed 2 M.png',link:`/product_categories/food and soup` },
-  { id: 3, name: 'Basket', image: '/gift-basket.avif',link:`/product_categories/basket` },
-  { id: 4, name: 'Drinks', image: 'close-up-wine-bottle-glass_23-2148673781.avif',link:`/product_categories/drinks` },
-   
+  {
+    id: 1,
+    name: "Home Appliances",
+    image: "/blender.avif",
+    link: `/product_categories/home appliences`,
+  },
+  {
+    id: 2,
+    name: "Food and Soup",
+    image: "/Whole Ogbonor seed 2 M.png",
+    link: `/product_categories/food and soup`,
+  },
+  {
+    id: 3,
+    name: "Basket",
+    image: "/gift-basket.avif",
+    link: `/product_categories/basket`,
+  },
+  {
+    id: 4,
+    name: "Drinks",
+    image: "/close-up-wine-bottle-glass_23-2148673781.avif",
+    link: `/product_categories/drinks`,
+  },
+  {
+    id: 5,
+    name: "Cooking Oil",
+    image: "/avocado pear oil.webp",
+    link: `/product_categories/cooking oil`,
+  },
+  {
+    id: 6,
+    name: "Grains and Pasta",
+    image: "/garri.avif",
+    link: `/product_categories/grains and pasta`,
+  },
 ];
 
 export default function ProductCategories() {
- 
-const router = useRouter()
+  const router = useRouter();
   return (
     <div className={classes.carouselWrapper}>
-      
-      
       <div className={classes.carouselContainer}>
-        <div   className={classes.carousel}>
+        <div className={classes.carousel}>
           {products.map((product) => (
-            <div key={product.id} className={classes.productCard} onClick={()=>{router.push(product.link)}}>
+            <div
+              key={product.id}
+              className={classes.productCard}
+              onClick={() => {
+                router.push(product.link);
+              }}
+            >
               <div className={classes.figure}>
-                <img src={product.image} alt={product.name} className={classes.productImage} width={242.7}  height={242.7}/>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className={classes.productImage}
+                  width={242.7}
+                  height={242.7}
+                />
               </div>
               <p className={classes.productName}>{product.name}</p>
             </div>
           ))}
         </div>
       </div>
-      
-      
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,11 @@ import BasketList from '@/component/basketList';
 import Layout from '@/component/layout/layout'; // Use Layout instead of directly using MainNavigation
 import ProductCarousel from '@/component/productCarousel';
 import ProductCategories from '@/component/productCategories';
+import NewArrivalsList from '@/component/newArrivalList';
 
 const Home = (props) => {
   return (
-    <Layout categories={props.categories} products={props.products}> 
+    <Layout categories={props.categories} products={props.products}>
       <Head>
         <title>..Household name in online supplies and services.</title>
       </Head>
@@ -31,38 +32,41 @@ const Home = (props) => {
         <BasketList basketProducts={props.basketProducts} />
       </div>
 
-    <div className={classes.carousel}>
-    <h3>Today's big deals</h3>
-    </div>
-      
-      <div  className={classes.ProductCarousel}>
-      <ProductCarousel products={props.products} />
+      <div className={classes.carousel}>
+        <h3>Today's big deals</h3>
       </div>
 
-    <div className={classes.categories}>
-    <h3>Shop Our Most Popular Categories</h3>
-    </div>
-      
-      <div  className={classes.Productcategories}>
-       <ProductCategories/>
+      <div className={classes.ProductCarousel}>
+        <ProductCarousel products={props.products} />
       </div>
-      
-  
-    
+
       <div className={classes.categories}>
-    <h3>Top Sellers</h3>
-    </div>
+        <h3>Shop Our Most Popular Categories</h3>
+      </div>
+
+      <div className={classes.Productcategories}>
+        <ProductCategories />
+      </div>
+
+      <section>
+        <div className={classes.categories}>
+          <h3>New Arrivals</h3>
+        </div>
+        <NewArrivalsList products={props.products} />
+      </section>
+
+      <div className={classes.categories}>
+        <h3>Top Sellers</h3>
+      </div>
       <ProductList products={props.products} />
       <div className={classes.productListTwo}>
-    <h3>Shop extraordinary items</h3>
-    </div>
+        <h3>Shop extraordinary items</h3>
+      </div>
       <ProductListTwo products={props.products} />
       <div className={classes.blogSection}>
-    <h3>Abiom Blog</h3>
-    </div>
-       <BlogSection />
-      
-     
+        <h3>Abiom Blog</h3>
+      </div>
+      <BlogSection />
     </Layout>
   );
 };
